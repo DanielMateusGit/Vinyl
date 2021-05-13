01c835ff4dc140b65541b004ff59fdbb9c06bab0
DanielMateusGit <62814149+DanielMateusGit@users.noreply.github.com>
2021-05-13 15:44:03 +0200

    Navigator Bar component

---
+++ lib/componenti/barra_navigazione.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class barra_navigazione extends StatelessWidget {
+  const barra_navigazione({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(
+          label: 'Music',
+          icon: Icon(Icons.headset_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: 'Poadcast',
+          icon: Icon(Icons.record_voice_over_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: 'Search',
+          icon: Icon(Icons.search_outlined),
+        ),
+        BottomNavigationBarItem(
+          label: 'Downloads',
+          icon: Icon(Icons.file_download),
+        ),
+      ],
+    );
+  }
+}
