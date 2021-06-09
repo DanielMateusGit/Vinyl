8023d2e12f54d7b13d6f5aa958c37b8810b1e8d1
DanielMateusGit <62814149+DanielMateusGit@users.noreply.github.com>
2021-06-09 23:16:03 +0200

    ricerca delle query da api youtube

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,18 +1,74 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:vinylproject/model/Video.dart';
+import 'package:http/http.dart' as http;
+
 
 class Search extends StatefulWidget {
-
-  static final String id = 'search_screen';
-
   @override
   _SearchState createState() => _SearchState();
 }
 
 class _SearchState extends State<Search> {
+
+  // id per la navigazione + queries di ricerca
+  static final String id = 'search_screen';
+  List<Video> _Queries =[];
+  String _Query;
+
+  // metodo per richiedere video da youtube
+  void fetchVideo() async {
+
+    _Queries.clear();
+    final response = await http.get(Uri.parse('https://www.googleapis.com/youtube/v3/search?part=snippet&q=$_Query&type=video&key=AIzaSyCBkLl_1dEmkxUEUdYJROlL9jMVDfBlCGY'));
+
+    if (response.statusCode == 200) {
+
+      var decodedData = jsonDecode(response.body);
+
+      for(int i= 0; i<5; i++){
+        _Queries.add(Video.fromJson(decodedData, i));
+      }
+
+      _Queries.reversed;
+
+      setState(() {});
+
+    } else {
+      throw Exception(response.body);
+
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Search"),
+    return Column(
+      children: [
+        TextField(
+          onChanged: (value) {
+            _Query = value;
+          },
+        ),
+        ElevatedButton(
+          onPressed: (){
+            fetchVideo();
+          },
+          child: Text('Cerca Video'),
+        ),
+        Expanded(
+          child: ListView.builder(
+            itemCount: _Queries.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: EdgeInsets.all(20.0),
+                child:
+                _Queries[index],
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
